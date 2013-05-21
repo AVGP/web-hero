cc3556631cf65d2fe6e6b53be569fd10f49faba8
Martin N <mr.avgp@googlemail.com>
2013-05-21 17:26:19 -0400

    Completed lesson 1, added (yet empty) lesson 2

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 angular.module("hero", []).config(function($routeProvider) {
     $routeProvider
     .when('/your-ship', { templateUrl: 'partials/1-your-ship.part.html' })
+    .when('/tech-trinity', { templateUrl: 'partials/2-tech-trinity.part.html' })
     .otherwise({
         templateUrl: 'partials/start.part.html'
     });
