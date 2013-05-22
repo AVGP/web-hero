8ab2093d9ce844d94c6c122bd20e9e975e19f356
Martin N <mr.avgp@googlemail.com>
2013-05-22 01:53:11 -0400

    Renaming partials again. Also rewriting the-orbit-1 slightly

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 angular.module("hero", []).config(function($routeProvider) {
     $routeProvider
     .when('/your-ship', { templateUrl: 'partials/1-your-ship.part.html' })
-    .when('/tech-trinity', { templateUrl: 'partials/2-tech-trinity.part.html' })
+    .when('/space-topography', { templateUrl: 'partials/2-space-topography.html' })
+    .when('/the-orbit', { templateUrl: 'partials/3-html-1.part.html' })
+    .when('/the-solar-system', { templateUrl: 'partials/4-css-1.part.html' })
     .otherwise({
         templateUrl: 'partials/start.part.html'
     });
