e6f09eda5d0b84672441fd27ff28b62db49b7c1d
Martin N <mr.avgp@googlemail.com>
2013-05-24 17:36:09 -0400

    Rewrote the tutorial to make it more fun.

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 angular.module("hero", []).config(function($routeProvider) {
     $routeProvider
     .when('/your-ship', { templateUrl: 'partials/1-your-ship.part.html' })
-    .when('/space-topography', { templateUrl: 'partials/2-space-topography.part.html' })
-    .when('/the-orbit', { templateUrl: 'partials/3-html-1.part.html' })
+    .when('/rocket-science-basics', { templateUrl: 'partials/2-rocket-science.part.html' })
+    .when('/our-ship', { templateUrl: 'partials/3-html-1.part.html' })
     .when('/the-cockpit', { templateUrl: 'partials/4-css-1.part.html' })
     .when('/decorate-our-ship', { templateUrl: 'partials/4-css-2.part.html' })
     .otherwise({
